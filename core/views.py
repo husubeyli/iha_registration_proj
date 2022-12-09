3608face35bcdb695db143ed37ac09fa390a45ff
husubeyli <husubayli@gmail.com>
2022-12-09 11:40:31 +0400

    l

--- core/views.py
+++ core/views.py
@@ -39,16 +39,17 @@ class IHAUpdateView(LoginRequiredMixin, generic.UpdateView):
 class IHAListView(generic.ListView):
     template_name = 'ihas/list.html'
     model = IHA
-    context_object_name = 'ihas'
+    context_object_name = 'ihas_list'
     ordering = '-created_at'
     paginate_by = 1
-    queryset = IHA.objects.all().select_related('markas', 'categories')
+    # queryset = IHA.objects.all().select_related('markas', 'categories')
 
     def get_queryset(self):
         queryset = super().get_queryset()
         querydict = self.request.GET
         # get request GET data filter query
         if querydict.get('categories', False):
+            print(list(map(int, str(querydict.get('categories')).split('-'))), 'salam')
             queryset = queryset.filter(categories__id__in=list(
                 map(int, str(querydict.get('categories')).split('-'))))
         if querydict.get('markas', False):
@@ -63,16 +64,19 @@ class IHAListView(generic.ListView):
 
         # search
         if querydict.get('query', False):
-            queryset = queryset.filter(name__contains=querydict['query'])
+            queryset = queryset.filter(name__icontains=querydict['query'])
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # get datat to template for when pagination changed and page reload data no changed get queryset data
         context["checkbox_categories"] = list(
-            map(int, str(self.request.GET.get('categories', '0')).split('-')))
+            map(int, str(self.request.GET.get('categories', 0)).split('-')))
         context["checkbox_markas"] = list(
-            map(int, str(self.request.GET.get('markas', '0')).split('-')))
+            map(int, str(self.request.GET.get('markas', 0)).split('-')))
+        context["filter_min_weight"] = self.request.GET.get('min_weight', False)
+        context["filter_max_weight"] = self.request.GET.get('max_weight', False)
         if self.request.GET.get('query', False):
-            print(self)
             context['search_input'] = self.request.GET['query']
+        
         return context
